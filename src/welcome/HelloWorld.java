62b88013b23fc1efe18b13bda66ce6015703b0ec
Menaya Mapalagama <Menaya@meghas-mbp.home>
2021-02-18 10:48:10 -0500

    My First Demo New Line

--- src/welcome/HelloWorld.java
+++ src/welcome/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Hello World!");
+		
+		System.out.println("This is next demo project line");
 
 	}
 
